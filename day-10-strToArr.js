1bec5b1ef8d3b2284599189cace5c076d1ff4b2c
tkarombang <tuangkarombang@gmail.com>
2024-02-07 22:34:06 +0800

    day 10/366

---
+++ day-10-strToArr.js
@@ -0,0 +1,16 @@
+// WPU Coding Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+
+// function stringToArray(string) {
+// return string.split(' ')
+
+// }
+
+// const stringToArray = (string) => string.split(' ');
+
+
+
+
+console.log(stringToArray('My Name is Azwar and I am not terorrist'));
