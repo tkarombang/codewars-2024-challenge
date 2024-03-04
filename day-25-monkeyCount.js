0acec4b4c727c2aa999820fd0ad7c8e986925a49
tkarombang <tuangkarombang@gmail.com>
2024-03-04 23:08:21 +0800

    day 25/366

---
+++ day-25-monkeyCount.js
@@ -0,0 +1,17 @@
+// WPU Coding Challenge 2024
+// 25/366
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+// function monkeyCount(n) {
+//   let arr = Array(n);
+//   console.log(arr)
+//   let result = [];
+//   for (let i = 1; i < arr.length + 1; i++){
+//     result.push(i)
+//   }
+//   return result;
+// }
+const monkeyCount = (n) => arr = [...Array(n)].map((_, a) => a + 1)
+
+
+console.log(monkeyCount(5))
