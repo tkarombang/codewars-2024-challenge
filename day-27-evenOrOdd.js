5f11a679fd2f610466f11d1d751504827d0c9720
tkarombang <tuangkarombang@gmail.com>
2024-03-09 15:10:39 +0800

    day 27/366

---
+++ day-27-evenOrOdd.js
@@ -0,0 +1,18 @@
+// WPU Coding Challenge 2024
+// 27/366
+// https://codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function evenOrOdd(number) {
+
+  return number % 2 == 0 ? "Even" : "Odd";
+
+  // if (number % 2 == 0){
+  //   return "Even"
+  // } else {
+  //   return "Odd"
+  // }
+
+}
+
+console.log(7 % 2)
+console.log(evenOrOdd())
