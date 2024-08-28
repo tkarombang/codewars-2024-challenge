7d169c44b06ca9db9aba8e81bfb9c5884c539e73
tkarombang <tuangkarombang@gmail.com>
2024-08-29 02:05:56 +0800

    Day 39/366

--- day-39-palindrome.js
+++ day-39-palindrome.js
@@ -2,8 +2,10 @@
 // 39/366
 // https://www.codewars.com/kata/57a1fd2ce298a731b20006a4
 
-function isPalindrom(x) {
-  return x.toLowerCase() === x.toLowerCase().split("").reverse().join("");
-}
+// function isPalindrom(x) {
+//   return x.toLowerCase() === x.toLowerCase().split("").reverse().join("");
+// }
+
+const isPalindrom = (x) => x.toLowerCase() === x.toLowerCase().split("").reverse().join("");
 
 console.log(isPalindrom("hello"));
