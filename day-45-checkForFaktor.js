7cd35c2369a3c53a23702787b88db2ba3a214a48
tkarombang <tuangkarombang@gmail.com>
2024-09-07 12:37:30 +0800

    Day 45/366

---
+++ day-45-checkForFaktor.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// 45/366
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+
+// function checkForFaktor(base, factor){
+// // return base % factor == 0;
+// return !(base % factor);
+// }
+
+const checkForFaktor = (base, factor) => !(base % factor);
+console.log(checkForFaktor(9, 2));
