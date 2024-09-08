ac77babdf3e7a8dc15a88e5b7801e59772a1d1a8
tkarombang <tuangkarombang@gmail.com>
2024-09-08 14:31:31 +0800

    Day 46/366

---
+++ day-46-twiceAsOld.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// 46/366
+// https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   // your code here
+//   return Math.abs(dadYearsOld - sonYearsOld * 2)
+// }
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2);
+
+console.log(twiceAsOld(55, 30));
