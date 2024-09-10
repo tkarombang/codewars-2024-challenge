bcd57f690efa71d135ef6fa194be76c5efe541e3
tkarombang <tuangkarombang@gmail.com>
2024-09-10 17:49:46 +0800

    Day 48/366

---
+++ day-48-cockroachSpeed.js
@@ -0,0 +1,10 @@
+// WPU Coding Challenge 2024
+// 47/366
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+
+function cockroachSpeed(s) {
+  //Good Luck!
+  return Math.floor((s * 100000) / 3600);
+}
+
+console.log(cockroachSpeed(1));
