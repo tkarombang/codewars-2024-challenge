cdacae28ed5bb0a103a890dd7accea68f7ccd15d
tkarombang <tuangkarombang@gmail.com>
2024-09-11 14:08:04 +0800

    Day 51/366

---
+++ day-51-removeEveryOther.js
@@ -0,0 +1,18 @@
+// WPU Coding Challenge 2024
+// 51/366
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+// function removeEveryOther(arr) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i += 2) {
+//     newArr.push(arr[i]);
+//   }
+//   return newArr;
+// }
+// function removeEveryOther(arr) {
+//   return arr.filter((el, i) => i % 2 == 0);
+// return arr.filter((el, i) => !(i & 1))
+// }
+const removeEveryOther = (arr) => arr.filter((el, i) => i % 2 == 0);
+
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"]));
