26f4e13888b9e4a172b4e412ee6d766906e4c315
tkarombang <tuangkarombang@gmail.com>
2024-10-12 22:47:35 +0800

    Day 65/366

--- day-65-creditCardMask.js
+++ day-65-creditCardMask.js
@@ -1,13 +1,16 @@
 // WPU Coding Challenge 2024
 // 65/366
 // https://www.codewars.com/kata/5412509bd436bd33920011bc
+// function maskify(cc) {
+//   let mask = "";
+//   for (let i = 0; i < cc.length - 4; i++) {
+//     mask += "#";
+//   }
+//   // console.log(mask += cc.slice(-4))
+//   return (mask += cc.slice(-4));
+// }
 function maskify(cc) {
-  let mask = "";
-  for (let i = 0; i < cc.length - 4; i++) {
-    mask += "#";
-  }
-  // console.log(mask += cc.slice(-4))
-  return (mask += cc.slice(-4));
+  return cc.slice(-4).padStart(cc.length, "#");
 }
 
 console.log(maskify("4556364607935616"));
