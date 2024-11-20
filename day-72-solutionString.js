10321571b80f1ad2f39fcec6dfe4875103cdf442
tkarombang <tuangkarombang@gmail.com>
2024-11-20 19:51:58 +0800

    Day 72/366

---
+++ day-72-solutionString.js
@@ -0,0 +1,39 @@
+// WPU Coding Challenge 2024
+// 72/366
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+// function solution(str, ending) {
+// console.log(str.length - ending.length)
+// if(ending.length > str.length){
+//   return false
+// }
+// for(let i = str.length - ending.length; i < str.length; i++){
+//   console.log(str[i])
+//   console.log(ending[i - (str.length - ending.length)])
+//   if(str[i] !== ending[i - (str.length - ending.length)]){
+//     return false
+//   }
+// }
+// return true
+
+// }
+// function solution(str, ending) {
+//   console.log(str.length - ending.length)
+//   console.log(str.includes('cde', 2))
+//   return str.includes(ending, str.length - ending.length)
+// }
+// console.log(solution('abcde', 'abc'))
+
+// 'abcde', 'cde' TRUE
+// 'abcde', 'abc' FALSE
+
+
+
+
+
+
+
+
+
+
+
